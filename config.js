313403f65a50bd0fb03c8d48418813bac54ad9a8
zartox93 <zartox738@gmail.com>
2025-03-05 17:04:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
+        buyer: '1042163409954803762', // Buyer
         couleur: '#6495ED', // Couleur HEX
         footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
